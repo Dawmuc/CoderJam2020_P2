9d467974f9c6472cc3ce3ebb976869f7bee2d2ad
Dawmuc <vincentpettineo@yahoo.fr>
2020-04-17 17:57:42 +0200

    Music + debug log removed

--- Coder Jam PL/Assets/Scripts/PlayerController.cs
+++ Coder Jam PL/Assets/Scripts/PlayerController.cs
@@ -378,7 +378,6 @@ public class PlayerController : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D col)
 	{
-		Debug.Log("hi");
 		isBeingThrown = false;
 		StopCoroutine(PlayerBounce());
 		StartCoroutine(PlayerBounce());
